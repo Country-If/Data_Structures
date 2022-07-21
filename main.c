e31510ae3d9df6e3b22cd16fc69c2d303c30c015
Country-If <2450854008@qq.com>
2022-07-21 23:32:35 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include "LinkList/SiLinkList_Head.h"
+#include "LinkList/SiLinkList_noHead.h"
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,21 @@
+//
+// Created by Maylon on 2022/7/21.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+#ifndef DATA_STRUCTURES_MAIN_H
+#define DATA_STRUCTURES_MAIN_H
+
+typedef int ElemType;
+
+/* enumerate function return status */
+typedef enum Status
+{
+    false,
+    true
+} Status;
+
+#endif //DATA_STRUCTURES_MAIN_H
