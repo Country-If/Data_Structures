f43e96ccdfe10c062c6d660900950d082179b103
Country-If <2450854008@qq.com>
2022-07-22 12:35:46 +0800

    finish functions: insert a node from head, traverse the list

--- LinkList/SiLinkList_Head.c
+++ LinkList/SiLinkList_Head.c
@@ -17,7 +17,19 @@ Status InitSiList_Head(SiList *L) {
 Status DestroySiList_Head(SiList *L) {}
 
 
-Status SiList_Head_Head_Insert(SiList L, ElemType e) {}
+Status SiList_Head_Head_Insert(SiList L, ElemType e) {
+    if (L == NULL) {
+        return false;
+    }
+    SiLNode *p = (SiLNode *)malloc(sizeof(SiLNode));
+    if (p == NULL) {
+        return false;
+    }
+    p->data = e;
+    p->next = L->next;
+    L->next = p;
+    return true;
+}
 
 
 Status SiList_Head_Tail_Insert(SiList L, ElemType e) {}
@@ -44,11 +56,20 @@ Status SiList_Head_Update_By_Value(SiList L, ElemType old, ElemType new) {}
 Status SiList_Head_Update_By_Order(SiList L, int i, ElemType e) {}
 
 
-void SiList_Head_Traverse(SiList L, void(*visit)(ElemType e)) {}
+void SiList_Head_Traverse(SiList L, void(*visit)(ElemType e)) {
+    SiList p = L->next;
+    while (p != NULL) {
+        visit(p->data);
+        p = p->next;
+    }
+    printf("NULL\n");
+}
 
 
 void silinklist_head_menu(void) {
     int choice = 0;
+    SiList L = NULL;
+    ElemType e;
     do {
         silinklist_head_menu_show_details();
         choice = judge_int();
@@ -56,23 +77,51 @@ void silinklist_head_menu(void) {
         switch (choice) {
             case 0:     // exit
                 break;
-            case 1:     //
+            case 1:     // Initialize
+                if (InitSiList_Head(&L)) {
+                    printf("Succeeded!\n");
+                }
+                else {
+                    printf("Failed!\n");
+                }
+                break;
+            case 2:     // Destroy
 
                 break;
-            case 2:     //
+            case 3:     // Insert a node from head
+                if (L == NULL) {
+                    printf("The list is NULL!\n");
+                }
+                else {
+                    get_input_element(&e);
+                    if (SiList_Head_Head_Insert(L, e)) {
+                        printf("Succeeded!\n");
+                    }
+                    else {
+                        printf("Failed!\n");
+                    }
+                }
+                break;
+            case 4:     // Insert a node from tail
 
                 break;
-            case 3:     //
+            case 5:     // Insert a node by order
 
                 break;
-            case 4:     //
+            case 6:     // Delete a node by order
 
                 break;
-            case 5:     //
+            case 7:     // Delete a node by value
 
                 break;
-            case 6:     //
+            case 8:     // Update a node by order
 
+                break;
+            case 9:     // Update a node by value
+
+                break;
+            case 10:    // Traverse the list
+                SiList_Head_Traverse(L, visit);
                 break;
             default:
                 printf("Wrong input, please re-enter.\n");
@@ -105,7 +154,9 @@ void silinklist_head_menu_show_details(void) {
     printf("\t*------------------------------------------------*\n");
     printf("\t*  9  |   Update a node by value                 *\n");
     printf("\t*------------------------------------------------*\n");
+    printf("\t*  10 |   Traverse the list                      *\n");
+    printf("\t*------------------------------------------------*\n");
     printf("\t*  0  |   Exit                                   *\n");
     printf("\t**************************************************\n");
-    printf("\nPlease enter the corresponding number(0-9): ");
+    printf("\nPlease enter the corresponding number(0-10): ");
 }
