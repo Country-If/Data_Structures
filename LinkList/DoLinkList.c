f0537b527a65eb8fc20d80bccf3f73f2fd32a57c
Country-If <2450854008@qq.com>
2022-07-31 20:08:01 +0800

    add function declarations

--- LinkList/DoLinkList.c
+++ LinkList/DoLinkList.c
@@ -4,7 +4,161 @@
 
 #include "DoLinkList.h"
 
-void dolinklist_menu(void) {}
+void InitDoList(DoList *L) {}
 
 
-void dolinklist_menu_show_details(void) {}
+Status DestroyDoList(DoList *L) {}
+
+
+Status DoList_Head_Insert(DoList *L, ElemType e) {}
+
+
+Status DoList_Tail_Insert(DoList *L, ElemType e) {}
+
+
+Status DoList_Insert_By_Order(DoList *L, int i, ElemType e) {}
+
+
+Status DoList_Delete_By_Value(DoList *L, ElemType e) {}
+
+
+Status DoList_Delete_By_Order(DoList *L, int i, ElemType *e) {}
+
+
+DoLNode *DoList_Retrieve_By_Value(DoList L, ElemType e) {}
+
+
+Status DoList_Update_By_Value(DoList L, ElemType old, ElemType new) {}
+
+
+Status DoList_Update_By_Order(DoList L, int i, ElemType e) {}
+
+
+void DoList_Traverse(DoList L, void(*visit)(ElemType e)) {}
+
+
+void dolinklist_menu(void) {
+    int choice;
+    DoList L = NULL;
+    ElemType e, old, new;
+    int i;
+    Status result;
+    do {
+        dolinklist_menu_show_details();
+        choice = judge_int();
+        system("cls");
+        switch (choice) {
+            case 0:     // exit
+                break;
+            case 1:     // Initialize
+
+                break;
+            case 2:     // Destroy
+                if (L == NULL) {
+                    printf("The list is already NULL!\n");
+                }
+                else {
+
+                }
+                break;
+            case 3:     // Insert a node from head
+                get_input_element(&e);
+                system("cls");
+
+                break;
+            case 4:     // Insert a node from tail
+                get_input_element(&e);
+                system("cls");
+
+                break;
+            case 5:     // Insert a node by order
+                get_order_position(&i);
+                get_input_element(&e);
+                system("cls");
+
+                break;
+            case 6:     // Delete a node by order
+                if (L == NULL) {
+                    printf("The list is NULL!\n");
+                }
+                else {
+                    get_order_position(&i);
+                    system("cls");
+
+                }
+                break;
+            case 7:     // Delete a node by value
+                if (L == NULL) {
+                    printf("The list is NULL!\n");
+                }
+                else {
+                    get_input_element(&e);
+                    system("cls");
+
+                }
+                break;
+            case 8:     // Update a node by order
+                if (L == NULL) {
+                    printf("The list is NULL!\n");
+                }
+                else {
+                    get_order_position(&i);
+                    get_input_element(&e);
+                    system("cls");
+
+                }
+                break;
+            case 9:     // Update a node by value
+                if (L == NULL) {
+                    printf("The list is NULL!\n");
+                }
+                else {
+                    get_input_element(&old);
+                    get_input_element(&new);
+                    system("cls");
+
+                }
+                break;
+            default:
+                printf("Wrong input, please re-enter!\n");
+                break;
+        }
+        if (choice >= 1 && choice <= 9) {
+            printf("Current list: ");
+
+        }
+    } while (choice != 0);
+    // release memory
+    if (L != NULL) {
+
+    }
+}
+
+
+void dolinklist_menu_show_details(void) {
+    printf("\n");
+    printf("\t**************************************************\n");
+    printf("\t*              Double Linked List                *\n");
+    printf("\t*------------------------------------------------*\n");
+    printf("\t*  1  |   Initialize (Reset)                     *\n");
+    printf("\t*------------------------------------------------*\n");
+    printf("\t*  2  |   Destroy                                *\n");
+    printf("\t*------------------------------------------------*\n");
+    printf("\t*  3  |   Insert a node from head                *\n");
+    printf("\t*------------------------------------------------*\n");
+    printf("\t*  4  |   Insert a node from tail                *\n");
+    printf("\t*-------------------------- ---------------------*\n");
+    printf("\t*  5  |   Insert a node by order                 *\n");
+    printf("\t*------------------------------------------------*\n");
+    printf("\t*  6  |   Delete a node by order                 *\n");
+    printf("\t*--------------------------- --------------------*\n");
+    printf("\t*  7  |   Delete a node by value                 *\n");
+    printf("\t*------------------------------------------------*\n");
+    printf("\t*  8  |   Update a node by order                 *\n");
+    printf("\t*------------------------------------------------*\n");
+    printf("\t*  9  |   Update a node by value                 *\n");
+    printf("\t*------------------------------------------------*\n");
+    printf("\t*  0  |   Back                                   *\n");
+    printf("\t**************************************************\n");
+    printf("\nPlease enter the corresponding number(0-9): ");
+}
--- LinkList/DoLinkList.h
+++ LinkList/DoLinkList.h
@@ -7,13 +7,113 @@
 #ifndef DATA_STRUCTURES_DOLINKLIST_H
 #define DATA_STRUCTURES_DOLINKLIST_H
 
+typedef struct DoLNode {
+    ElemType data;
+    struct DoLNode *next;
+} DoLNode, *DoList;
+
+/* Create */
 /*!
- * The single linked list (with a head node) menu
+ * Initialize or reset the double linked list
+ * @param L : pointer to the double linked list pointer
+ */
+void InitDoList(DoList *L);
+
+/* Destroy */
+/*!
+ * Destroy the double linked list
+ * @param L : pointer to the double linked list pointer
+ * @return status (true, false)
+ */
+Status DestroyDoList(DoList *L);
+
+/* Insert */
+/*!
+ * Insert a node from head of the double linked list
+ * @param L : pointer to the double linked list pointer
+ * @param e : element to be inserted
+ * @return status (true, false)
+ */
+Status DoList_Head_Insert(DoList *L, ElemType e);
+
+/*!
+ * Insert a node from tail of the double linked list
+ * @param L : pointer to the double linked list pointer
+ * @param e : element to be insert
+ * @return status (true, false)
+ */
+Status DoList_Tail_Insert(DoList *L, ElemType e);
+
+/*!
+ * Insert a node before a specific node of the double linked list by order
+ * @param L : pointer to the double linked list pointer
+ * @param i : the order of a specific node, legitimate range: [1, n + 1]
+ * @param e : element to be insert
+ * @return status (true, false, input_error)
+ */
+Status DoList_Insert_By_Order(DoList *L, int i, ElemType e);
+
+/* Delete */
+/*!
+ * Delete a node of the double linked list by value
+ * @param L : pointer to the double linked list pointer
+ * @param e : the data of the node to be deleted
+ * @return status (true, false)
+ */
+Status DoList_Delete_By_Value(DoList *L, ElemType e);
+
+/*!
+ * Delete a node of the double linked list by order
+ * @param L : pointer to the double linked list pointer
+ * @param i : the order of a specific node, legitimate range: [1, n]
+ * @param e : the data of the node to be deleted
+ * @return status (true, false, input_error)
+ */
+Status DoList_Delete_By_Order(DoList *L, int i, ElemType *e);
+
+/* Retrieve */
+/*!
+ * Retrieve a node of the double linked list by value
+ * @param L : pointer to a double linked list
+ * @param e : the data of the node to be retrieve
+ * @return pointer to the result node if successfully retrieved else NULL
+ */
+DoLNode *DoList_Retrieve_By_Value(DoList L, ElemType e);
+
+/* Update */
+/*!
+ * Update a node of the double linked list by value
+ * @param L : pointer to a double linked list
+ * @param old : the old data of the node to be updated
+ * @param new : the new data of the node to be updated
+ * @return status (true, false)
+ */
+Status DoList_Update_By_Value(DoList L, ElemType old, ElemType new);
+
+/*!
+ * Update a node of the double linked list by order
+ * @param L : pointer to a double linked list
+ * @param i : the order of a specific node, legitimate range: [1, n]
+ * @param e : the data of the node to be updated
+ * @return status (true, false, input_error)
+ */
+Status DoList_Update_By_Order(DoList L, int i, ElemType e);
+
+/* Traverse */
+/*!
+ * Traverse the double linked list
+ * @param L : pointer to a double linked list
+ * @param visit : function pointer to a function that prints the data of a node
+ */
+void DoList_Traverse(DoList L, void(*visit)(ElemType e));
+
+/*!
+ * The double linked list menu
  */
 void dolinklist_menu(void);
 
 /*!
- * The single linked list (with a head node) menu details
+ * The double linked list menu details
  */
 void dolinklist_menu_show_details(void);
 
