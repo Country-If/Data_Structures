f6715f52bf4583eda6f77147befc14ec34c2b172
Maylon <2450854008@qq.com>
2022-09-03 21:37:52 +0800

    add skeleton of linked queue

--- Queue/LinkQueue.c
+++ Queue/LinkQueue.c
@@ -4,4 +4,147 @@
 
 #include "LinkQueue.h"
 
+Status InitLinkQueue(LinkQueue *Q) {}
 
+
+void DestroyLinkQueue(LinkQueue *Q) {}
+
+
+Status EnLinkQueue(LinkQueue *Q, ElemType e) {}
+
+
+Status DeLinkQueue(LinkQueue *Q, ElemType *e) {}
+
+
+void LinkQueue_Get_Head(LinkQueue Q, ElemType *e) {}
+
+
+Status LinkQueue_Empty(LinkQueue Q) {}
+
+
+int LinkQueue_Len(LinkQueue Q) {}
+
+
+void LinkQueue_Traverse(LinkQueue Q, void(*visit)(ElemType e)) {}
+
+
+void linkqueue_menu(void) {
+    int choice;
+    LinkQueue Q = NULL;
+    ElemType e;
+    Status result;
+    do {
+        linkqueue_menu_show_details();
+        choice = judge_int();
+        system("cls");
+        switch (choice) {
+            case 0:     // exit
+                break;
+            case 1:     // Initialize
+                if (InitLinkQueue(&Q) == true) {
+                    printf("Succeeded!\n");
+                    printf("Current queue (from front to rear): NULL\n");
+                }
+                else {
+                    printf("Failed!\n");
+                }
+                break;
+            case 2:     // Destroy
+                if (Q == NULL) {
+                    printf("The stack isn't initialized!\n");
+                }
+                else {
+                    DestroyLinkQueue(&Q);
+                    printf("Succeeded!\n");
+                }
+                break;
+            case 3:     // EnQueue
+                if (Q == NULL) {
+                    printf("The stack isn't initialized!\n");
+                }
+                else {
+                    get_input_element(&e);
+                    system("cls");
+                    result = EnLinkQueue(&Q, e);
+                    if (result == true) {
+                        printf("Succeeded!\n");
+                    }
+                    else {
+                        printf("Failed!\n");
+                    }
+                }
+                break;
+            case 4:     // DeQueue
+                if (Q == NULL) {
+                    printf("The stack isn't initialized!\n");
+                }
+                else if (LinkQueue_Empty(Q) == true) {
+                    printf("The stack is NULL!\n");
+                }
+                else {
+                    result = DeLinkQueue(&Q, &e);
+                    if (result == true) {
+                        printf("Successfully deleted data: %d\n", e);
+                    }
+                    else {
+                        printf("Failed!\n");
+                    }
+                }
+                break;
+            case 5:     // Get head
+                if (Q == NULL) {
+                    printf("The stack isn't initialized!\n");
+                }
+                else if (LinkQueue_Empty(Q) == true) {
+                    printf("The stack is NULL!\n");
+                }
+                else {
+                    LinkQueue_Get_Head(Q, &e);
+                    printf("Stack Top data: %d\n", e);
+                }
+                break;
+            case 6:     // Get length
+                if (Q == NULL) {
+                    printf("The stack isn't initialized!\n");
+                }
+                else {
+                    printf("Stack length: %d\n", LinkQueue_Len(Q));
+                }
+                break;
+            default:
+                printf("Wrong input, please re-enter!\n");
+                break;
+        }
+        if (choice >= 3 && choice <= 6 && Q != NULL) {
+            printf("Current queue (from front to rear): ");
+            LinkQueue_Traverse(Q, visit);
+        }
+    } while (choice != 0);
+    // release memory
+    if (Q != NULL) {
+        DestroyLinkQueue(&Q);
+    }
+}
+
+
+void linkqueue_menu_show_details(void) {
+    printf("\n");
+    printf("\t********************************************\n");
+    printf("\t*       Linked Queue (with head)           *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  1  |   Initialize (Reset)               *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  2  |   Destroy                          *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  3  |   EnQueue                          *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  4  |   DeQueue                          *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  5  |   Get head                         *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  6  |   Get length                       *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  0  |   Back                             *\n");
+    printf("\t********************************************\n");
+    printf("\nPlease enter the corresponding number(0-6): ");
+}
--- Queue/LinkQueue.h
+++ Queue/LinkQueue.h
@@ -7,6 +7,74 @@
 #ifndef DATA_STRUCTURES_LINKQUEUE_H
 #define DATA_STRUCTURES_LINKQUEUE_H
 
+typedef struct LinkQueueNode {
+    ElemType data;
+    struct LinkQueueNode *next;
+} LinkQueueNode, *LinkQueue;
+
+/* Create */
+/*!
+ * Initialize or reset the linked queue (with a head)
+ * @param Q : pointer to the linked queue
+ * @return status (true, false)
+ */
+Status InitLinkQueue(LinkQueue *Q);
+
+/* Destroy */
+/*!
+ * Destroy the linked queue (with a head)
+ * @param Q : pointer to the linked queue
+ */
+void DestroyLinkQueue(LinkQueue *Q);
+
+/* EnQueue */
+/*!
+ * Enter an element to the rear of the linked queue (with a head)
+ * @param Q : pointer to the linked queue
+ * @param e : the data to be entered
+ * @return status (true, false)
+ */
+Status EnLinkQueue(LinkQueue *Q, ElemType e);
+
+/* DeQueue */
+/*!
+ * Delete an element from the front of the linked queue (with a head)
+ * @param Q : pointer to the linked queue
+ * @param e : pointer to the data to be deleted
+ * @return status (true, false)
+ */
+Status DeLinkQueue(LinkQueue *Q, ElemType *e);
+
+/* Retrieve */
+/*!
+ * Get the head data of the linked queue (with a head)
+ * @param Q : the linked queue
+ * @param e : pointer to the head data
+ */
+void LinkQueue_Get_Head(LinkQueue Q, ElemType *e);
+
+/*!
+ * Judge if the linked queue (with a head) is empty
+ * @param Q : the linked queue
+ * @return status (true, false)
+ */
+Status LinkQueue_Empty(LinkQueue Q);
+
+/*!
+ * Get the length of the linked queue (with a head)
+ * @param Q : the linked queue
+ * @return the length of the queue
+ */
+int LinkQueue_Len(LinkQueue Q);
+
+/* Traverse */
+/*!
+ * Traverse the linked queue (with a head)
+ * @param Q : the linked queue
+ * @param visit : function pointer to the function that prints the data of a node
+ */
+void LinkQueue_Traverse(LinkQueue Q, void(*visit)(ElemType e));
+
 /*!
  * The linked queue (with a head) menu
  */
