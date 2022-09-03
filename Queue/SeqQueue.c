e66c4e0f045af7c2e9891d2020570d5c056a1671
Maylon <2450854008@qq.com>
2022-09-03 20:45:34 +0800

    add skeleton of sequential queue

--- Queue/SeqQueue.c
+++ Queue/SeqQueue.c
@@ -4,4 +4,163 @@
 
 #include "SeqQueue.h"
 
+Status InitSeqQueue(SeqQueue *Q) {
+    (*Q).data = (ElemType *) calloc(DeltaSize, sizeof(ElemType));
+    if ((*Q).data == NULL) {
+        return false;
+    }
 
+    (*Q).front = (*Q).rear = 0;
+    return true;
+}
+
+
+Status SeqQueue_Increase_Capacity(SeqQueue *Q) {}
+
+
+void DestroySeqQueue(SeqQueue *Q) {
+    free((*Q).data);
+}
+
+
+Status EnSeqQueue(SeqQueue *Q, ElemType e) {}
+
+
+Status DeSeqQueue(SeqQueue *Q, ElemType *e) {}
+
+
+void SeqQueue_Get_Head(SeqQueue Q, ElemType *e) {}
+
+
+Status SeqQueue_Empty(SeqQueue Q) {}
+
+
+int SeqQueue_Len(SeqQueue Q) {}
+
+
+void SeqQueue_Traverse(SeqQueue Q, void(*visit)(ElemType e)) {}
+
+
+void seqqueue_menu(void) {
+    int choice;
+    SeqQueue Q;
+    int init_flag = 0;
+    ElemType e;
+    Status result;
+    do {
+        seqqueue_menu_show_details();
+        choice = judge_int();
+        system("cls");
+        switch (choice) {
+            case 0:     // exit
+                break;
+            case 1:     // Initialize
+                if (InitSeqQueue(&Q) == true) {
+                    init_flag = 1;
+                    printf("Succeeded!\n");
+                    printf("Current queue (from front to rear): NULL\n");
+                }
+                else {
+                    printf("Failed!\n");
+                }
+                break;
+            case 2:     // Destroy
+                if (init_flag == 0) {
+                    printf("The stack isn't initialized!\n");
+                }
+                else {
+                    DestroySeqQueue(&Q);
+                    init_flag = 0;
+                    printf("Succeeded!\n");
+                }
+                break;
+            case 3:     // EnQueue
+                if (init_flag == 0) {
+                    printf("The stack isn't initialized!\n");
+                }
+                else {
+                    get_input_element(&e);
+                    system("cls");
+                    result = EnSeqQueue(&Q, e);
+                    if (result == true) {
+                        printf("Succeeded!\n");
+                    }
+                    else {
+                        printf("Failed!\n");
+                    }
+                }
+                break;
+            case 4:     // DeQueue
+                if (init_flag == 0) {
+                    printf("The stack isn't initialized!\n");
+                }
+                else if (SeqQueue_Empty(Q) == true) {
+                    printf("The stack is NULL!\n");
+                }
+                else {
+                    result = DeSeqQueue(&Q, &e);
+                    if (result == true) {
+                        printf("Successfully deleted data: %d\n", e);
+                    }
+                    else {
+                        printf("Failed!\n");
+                    }
+                }
+                break;
+            case 5:     // Get head
+                if (init_flag == 0) {
+                    printf("The stack isn't initialized!\n");
+                }
+                else if (SeqQueue_Empty(Q) == true) {
+                    printf("The stack is NULL!\n");
+                }
+                else {
+                    SeqQueue_Get_Head(Q, &e);
+                    printf("Stack Top data: %d\n", e);
+                }
+                break;
+            case 6:     // Get length
+                if (init_flag == 0) {
+                    printf("The stack isn't initialized!\n");
+                }
+                else {
+                    printf("Stack length: %d\n", SeqQueue_Len(Q));
+                }
+                break;
+            default:
+                printf("Wrong input, please re-enter!\n");
+                break;
+        }
+        if (choice >= 3 && choice <= 6 && init_flag != 0) {
+            printf("Current queue (from front to rear): ");
+            SeqQueue_Traverse(Q, visit);
+        }
+    } while (choice != 0);
+    // release memory
+    if (init_flag != 0) {
+        DestroySeqQueue(&Q);
+    }
+}
+
+
+void seqqueue_menu_show_details(void) {
+    printf("\n");
+    printf("\t********************************************\n");
+    printf("\t*          Sequential Queue                *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  1  |   Initialize (Reset)               *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  2  |   Destroy                          *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  3  |   EnQueue                          *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  4  |   DeQueue                          *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  5  |   Get head                         *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  6  |   Get length                       *\n");
+    printf("\t*------------------------------------------*\n");
+    printf("\t*  0  |   Back                             *\n");
+    printf("\t********************************************\n");
+    printf("\nPlease enter the corresponding number(0-6): ");
+}
--- Queue/SeqQueue.h
+++ Queue/SeqQueue.h
@@ -7,6 +7,83 @@
 #ifndef DATA_STRUCTURES_SEQQUEUE_H
 #define DATA_STRUCTURES_SEQQUEUE_H
 
+#define DeltaSize 5
+
+typedef struct {
+    ElemType *data;
+    int front, rear;
+} SeqQueue;
+
+/* Create */
+/*!
+ * Initialize or reset the sequential queue
+ * @param Q : pointer to the sequential queue
+ * @return status (true, false)
+ */
+Status InitSeqQueue(SeqQueue *Q);
+
+/*!
+ * Increase capacity of the sequential queue when the queue is full
+ * @param Q : pointer to the sequential queue
+ * @return status (true, false)
+ */
+Status SeqQueue_Increase_Capacity(SeqQueue *Q);
+
+/* Destroy */
+/*!
+ * Destroy the sequential queue
+ * @param Q : pointer to the sequential queue
+ */
+void DestroySeqQueue(SeqQueue *Q);
+
+/* EnQueue */
+/*!
+ * Enter an element to the rear of the sequential queue
+ * @param Q : pointer to the sequential queue
+ * @param e : the data to be entered
+ * @return status (true, false)
+ */
+Status EnSeqQueue(SeqQueue *Q, ElemType e);
+
+/* DeQueue */
+/*!
+ * Delete an element from the front of the sequential queue
+ * @param Q : pointer to the sequential queue
+ * @param e : pointer to the data to be deleted
+ * @return status (true, false)
+ */
+Status DeSeqQueue(SeqQueue *Q, ElemType *e);
+
+/* Retrieve */
+/*!
+ * Get the head data of the sequential queue
+ * @param Q : the sequential queue
+ * @param e : pointer to the head data
+ */
+void SeqQueue_Get_Head(SeqQueue Q, ElemType *e);
+
+/*!
+ * Judge if the sequential queue is empty
+ * @param Q : the sequential queue
+ * @return status (true, false)
+ */
+Status SeqQueue_Empty(SeqQueue Q);
+
+/*!
+ * Get the length of the sequential queue
+ * @param Q : the sequential queue
+ * @return the length of the queue
+ */
+int SeqQueue_Len(SeqQueue Q);
+
+/* Traverse */
+/*!
+ * Traverse the sequential queue
+ * @param Q : the sequential queue
+ * @param visit : function pointer to the function that prints the data of a node
+ */
+void SeqQueue_Traverse(SeqQueue Q, void(*visit)(ElemType e));
+
 /*!
  * The sequential queue menu
  */
